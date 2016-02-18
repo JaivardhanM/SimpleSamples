90738f412d1ee27c9b43e0fc49fad65c8839b489
JaivardhanM <jaivardhan.mattapalli@gmail.com>
2016-02-18 09:32:10 -0500

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,13 @@
+# FILE: HelloWorld.py
+# Owner: Jaivardhan Mattapalli
+# Date: 5/18/2015
 
+# Start of Main 
+def main():
+  print "Hello World" # Prints Hello World on console
+
+
+# Standard boilerplate to call the main() function.
+if __name__ == '__main__':
+  main()
+#EndOfFile
